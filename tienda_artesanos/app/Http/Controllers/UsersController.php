90d5e14d563fad401c322484ed274011a86dea5a
jmarfaj10 <jmarfaj10@gmail.com>
2026-04-25 14:32:44 +0200

    update and delete implemented

--- tienda_artesanos/app/Http/Controllers/UsersController.php
+++ tienda_artesanos/app/Http/Controllers/UsersController.php
@@ -37,6 +37,7 @@ class UsersController extends Controller
 
         try{
             $request->validate([
+                'username' => 'required|string|max:20',
                 'name' => 'required|string|max:20',
                 'email' => 'required|email|unique:users,email',
                 'password' => 'required|min:8|confirmed',
@@ -46,13 +47,14 @@ class UsersController extends Controller
 
             $role=Role::find($request->role);
             $newUser = new User;
+            $newUser -> username = $request -> username;
             $newUser -> name = $request -> name;
             $newUser -> email = $request -> email;
             $newUser -> password = Hash::make($request->password);
             $newUser -> phone = $request -> phone;
             $newUser -> address = $request -> address;
             $newUser->saveOrFail();
-            
+
             if(!$role){
                 $newException = new Exception("rol no existe");
                 throwException($newException);
@@ -71,7 +73,28 @@ class UsersController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try{
+            $users = $this->get_user($id);
+        }catch(Exception $e){
+            $msg = $e->getMessage();
+            return view('listUsers', compact('msg'));
+        }
+        
+        return view('listUsers', compact('users'));
+    }
+
+    public function show_by_username(string $username)
+    {
+        $user = User::where('username', $username)->first();
+        if(!$user){
+            return view('listUsers', [
+            'users' => [], 
+            'msg' => "Usuario no encontrado"
+            ]);
+        }
+
+        $users = [$user];
+        return view('listUsers', compact('users'));
     }
 
     /**
@@ -79,22 +102,96 @@ class UsersController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        try{
+            $user = $this->get_user($id);
+            $roles = Role::all();
+            return view('editUser', compact('user', 'roles'));
+        }catch(Exception $e){
+            return view('listUsers', ['users' => [], 'msg' => $e->getMessage()]);
+        }
+        
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
-    {
-        //
+    {   
+    try {
+        $user = $this->get_user($id);
+
+        $rules = [
+            'username' => 'required|string|max:20',
+            'name'     => 'required|string|max:20',
+            'email'    => 'required|email|unique:users,email,' . $id, 
+            'phone'    => 'nullable|string|min:9',
+            'address'  => 'nullable|string|min:18'
+        ];
+
+        if ($request->filled('password')) {
+            $rules['password'] = 'required|min:8|confirmed';
+        }
+
+        $request->validate($rules);
+
+        $user->username = $request->username;
+        $user->name     = $request->name;
+        $user->email    = $request->email;
+        $user->phone    = $request->phone;
+        $user->address  = $request->address;
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->save();
+
+        $role = Role::find($request->role);
+        if (!$role) {
+            throw new Exception("El rol seleccionado no existe");
+        }
+
+        $user->roles()->sync($request->role);
+
+    } catch (\Throwable $e) {
+        dd([
+        'Mensaje' => $e->getMessage(),
+        'Archivo' => $e->getFile(),
+        'Linea'   => $e->getLine(),
+        'Datos_Enviados' => $request->all()
+    ]);
     }
+
+    return redirect('/users')->with('msg', 'Usuario actualizado con éxito');
+}
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $user = $this->get_user($id);
+            $user->delete();
+            
+        } catch (\Throwable $e) {
+        dd([
+        'Mensaje' => $e->getMessage(),
+        'Archivo' => $e->getFile(),
+        'Linea'   => $e->getLine(),
+        ]);
+        }
+
+        return redirect('/users')->with('msg', 'Usuario eliminado con éxito');
+    }
+
+    public function get_user(string $id)
+    {
+        $user = User::find($id);
+        if(!$user){
+            throw new Exception("Usuario no existe");
+        }
+        return $user;
     }
 }
--- tienda_artesanos/routes/web.php
+++ tienda_artesanos/routes/web.php
@@ -14,3 +14,13 @@ Route::get('/users', [UsersController::class, 'index']);
 Route::get('/createUsers', [UsersController::class, 'create'])->name('users.create');
 
 Route::post('/createUsers', [UsersController::class, 'store'])->name('users.store');
+
+Route::get("/users/{id}", [UsersController::class, 'show'])->name('users.show')->where('id', '[0-9]+');
+
+Route::get("/users/{username}", [UsersController::class, 'show_by_username'])->name('users.show_by_name');
+
+Route::get('/editUsers/{id}', [UsersController::class, 'edit'])->name('users.edit');
+
+Route::put('/updateUsers/{id}', [UsersController::class, 'update'])->name('users.update');
+
+Route::delete('/deleteUsers/{id}', [UsersController::class, 'destroy'])->name('users.delete');
